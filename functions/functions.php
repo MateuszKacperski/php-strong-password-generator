3c97357afa181e7b28eeb53f7882394c3fccc39b
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-02-21 16:49:49 +0100

    fixing

--- functions/functions.php
+++ functions/functions.php
@@ -1,14 +1,12 @@
 <?php
 
 function passwordRandom($lunghezza_password) {
-    $pass = [];
-    $caracter_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!', '"', '£', '$', '%', '&', '/', '(', ')', '=', '?', '^', '*', '[', ']'];
-
-    $length = count($caracter_array) - 1 ;
-    for($i=0; $i > $lunghezza_password; $i++) {
-        $caracter = rand(0, $length);
-        $pass[] = $caracter_array[$caracter];
+    $pass = '';
+    $caracter = 'abcdefghijklmnopqrstuvwxyz0123456789!"£$%&/()=?^*[]';
+    for($i=0; $i < $lunghezza_password; $i++) {
+        $n = rand(0, strlen($caracter) - 1);
+        $pass .= $caracter[$n];
     }
-    return implode($pass);
+    return $pass;
 }
 ?>
