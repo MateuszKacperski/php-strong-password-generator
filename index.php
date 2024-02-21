d6e5218959334c1b4e3c5afe5aa4ea922d69100a
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-02-21 15:53:25 +0100

    milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php 
+
+//Recupero password
 $lunghezza_password = $_GET['password'] ?? '';
+
+//Creo array con tutti i possibili simboli 
+$caracter_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','!', '"', '£', '$', '%', '&', '/', '(', ')', '=', '?', '^', '*', '[', ']'];
+
+//Mi ricavo la lunghezza totale dellàrray di caratteri
+$length = count($caracter_array) - 1 ;
+
+//genero la password
+for($i=0; $i > $lunghezza_password; $i++) {
+    $caracter = rand(0, $length);
+    $pass[] = $caracter_array[$caracter];
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +40,10 @@ $lunghezza_password = $_GET['password'] ?? '';
                     </div>
                     <button type="submit" class="btn btn-primary">Invia</button>
                      <button type="reset" class="btn btn-secondary">Annulla</button>
-
+            </div>
+            <div class="my-5 text-center">
+                <h3>La password è:</h3>
+                <p><?php  echo $pass ?></p>
             </div>
         </form>
     </div>
